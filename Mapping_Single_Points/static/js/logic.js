9a019ff426f8c2309558e724294b16a23c2a2983
smucnyj13104 <90653897+smucnyj13104@users.noreply.github.com>
2021-12-03 19:03:58 -0800

    mapping single points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //We're assigning the variable map to the object L.map(), and we'll instantiate the object with the given string 'mapid'.
 //The mapid will reference the id tag in our <div> element on the index.html file.
@@ -10,12 +10,12 @@ console.log("working");
 //and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.
 
 // Create the map object with a center and zoom level (alternative to above)
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-});
+//let map = L.map("mapid", {
+  //  center: [
+    //  40.7, -94.5
+    //],
+    //zoom: 4/
+//});
 
 // We create the tile layer that will be the background of our map (from leaflet quickstart guide)
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -28,11 +28,31 @@ let map = L.map("mapid", {
 //});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//let marker = L.circle([34.0522, -118.2437], {
+  //radius: 100
+//}).addTo(map);
+
+let marker = L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  //  maxZoom: 18,
+    //accessToken: API_KEY
+//});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
